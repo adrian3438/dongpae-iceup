631ab10b7cbbabb458c4e2d472c327cd68b1fdff
adrian3438 <admin@bmcnet.kr>
2024-10-17 17:01:10 +0900

    modify

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ app.prepare().then(() => {
         return handle(req, res);
     });
 
-    const port = process.env.PORT || 3003;
+    const port = process.env.PORT || 3000;
 
     connection.connect((err) => {
         if (err) {
