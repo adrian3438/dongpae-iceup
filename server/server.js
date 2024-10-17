db1e2e14dfbff046bdad8320b508a65fe0630705
adrian3438 <admin@bmcnet.kr>
2024-10-17 18:13:35 +0900

    prot number 변경

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ app.prepare().then(() => {
         return handle(req, res);
     });
 
-    const port = process.env.PORT || 3000;
+    const port = process.env.PORT || 3001;
 
     connection.connect((err) => {
         if (err) {
