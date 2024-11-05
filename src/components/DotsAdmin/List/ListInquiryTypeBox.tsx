4b35926e59f9f49a2ed4b63df39d2af5ee07aebe
jdlee <jdlee@bmcnet.kr>
2024-11-05 13:36:56 +0900

    관리자 수정

--- src/components/DotsAdmin/List/ListInquiryTypeBox.tsx
+++ src/components/DotsAdmin/List/ListInquiryTypeBox.tsx
@@ -17,11 +17,11 @@ export default function ListInquiryTypeBox ({inquiryType} : Props) {
         <div className="selectBox">
             <select value={inquiryType} name="" id="" onChange={search}>
                 <option value="ALL" selected>전체</option>
-                <option value="S" selected>스마일리더 랩</option>
-                <option value="T" selected>테스터</option>
-                <option value="N" selected>영양제</option>
-                <option value="F" selected>식품</option>
-                <option value="O" selected>기타</option>
+                <option value="S" selected>기업용 제빙기</option>
+                <option value="T" selected>대리점 모집</option>
+                <option value="N" selected>해외수출</option>
+                <option value="F" selected>투자문의</option>
+                <option value="O" selected>기타문의</option>
             </select>
         </div>
         </>
